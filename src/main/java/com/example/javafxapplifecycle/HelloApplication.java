6b83e5ae82ea049a354b283817b78b71c801bcb3
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-09-27 23:21:16 -0400

    JavaFX: App Life Cycle

--- src/main/java/com/example/javafxapplifecycle/HelloApplication.java
+++ src/main/java/com/example/javafxapplifecycle/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.example.javafxapplifecycle;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +12,28 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        System.out.println("Start begins");
+
+        stage.setTitle("Life Cycle");
+
+        FlowPane rootFlowPane = new FlowPane(Orientation.VERTICAL, 5, 5);
+        rootFlowPane.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(rootFlowPane, 700, 400);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        System.out.println("Init begins");
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println("Stop begins");
     }
 
     public static void main(String[] args) {
